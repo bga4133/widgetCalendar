93c3cca3fa82aa1e8003f52c7a84455746150a08
bgadev12 <bgarcia6479@misena.edu.co>
2018-10-04 18:25:36 -0500

    firts commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+new TinyPicker({
+    firstBox:document.getElementById('startDate'),
+    startDate: new Date,
+    months: 2,
+    days: ['Lu','Ma','Mi','Ju','Vi','Sa','Do'],
+    local: 'en-US',
+    allowPast: true,
+}).init();
+new TinyPicker({
+    firstBox:document.getElementById('startDate1'),
+    startDate: new Date,
+    endDate: new Date,
+    lastBox: document.getElementById('endDate1'),
+    months: 3,
+    days: ['Lu','Ma','Mi','Ju','Vi','Sa','Do'],
+    local: 'en-US',
+    err: function(){alert('err');}
+}).init();
+
